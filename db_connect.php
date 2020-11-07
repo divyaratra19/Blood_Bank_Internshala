22dfa1170a80d5704efff173bba3a78fd442d166
Divya Ratra <33549893+divyaratra19@users.noreply.github.com>
2020-11-07 23:56:43 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$connection= mysqli_connect('localhost','root','');
-$db=mysqli_select_db($connection,'internshala');
+$connection= mysqli_connect('remotemysql.com','0QDnkFWtQc','SkhVP7FIdE');
+$db=mysqli_select_db($connection,'0QDnkFWtQc');
 if(!$connection)
 {
     $_SESSION['status']= "Database Connection error";
